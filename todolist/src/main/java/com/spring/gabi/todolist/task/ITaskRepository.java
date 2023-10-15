8344bb2671494ccbb82b068f00648ede76539b24
Gabriela Singulani <gabriela.singulani@ice.ufjf.br>
2023-10-15 14:52:30 -0300

    Listagem de tarefas pelo userId

--- todolist/src/main/java/com/spring/gabi/todolist/task/ITaskRepository.java
+++ todolist/src/main/java/com/spring/gabi/todolist/task/ITaskRepository.java
@@ -1,9 +1,10 @@
 package com.spring.gabi.todolist.task;
 
+import java.util.List;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ITaskRepository extends JpaRepository<TaskModel, UUID>{
-    
+    List<TaskModel> findByUserId(UUID userId);
 }
