6dc68eed5a2a852d11c7eaba44cbf73617f0456a
Gabriela Singulani <gabriela.singulani@ice.ufjf.br>
2023-10-15 10:40:11 -0300

    Classe IUserRepository criada

--- todolist/src/main/java/com/spring/gabi/todolist/user/UserController.java
+++ todolist/src/main/java/com/spring/gabi/todolist/user/UserController.java
@@ -1,5 +1,6 @@
 package com.spring.gabi.todolist.user;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,10 +9,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/user")
 public class UserController {
+    @Autowired
+    private IUserRepository userRepository;
 
     @PostMapping("/")
-    public void create(@RequestBody UserModel userModel){
-        System.out.println(userModel.getUserName());
+    public UserModel create(@RequestBody UserModel userModel){
+        var userCreated = this.userRepository.save(userModel);
+        return userCreated;
     }
     
 }
