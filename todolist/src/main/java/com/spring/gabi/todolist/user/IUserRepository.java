921aa30b90e415b4a75c0b0a8f445773910dd434
Gabriela Singulani <gabriela.singulani@ice.ufjf.br>
2023-10-15 14:27:37 -0300

    Repository das tarefas

--- todolist/src/main/java/com/spring/gabi/todolist/user/IUserRepository.java
+++ todolist/src/main/java/com/spring/gabi/todolist/user/IUserRepository.java
@@ -2,7 +2,6 @@ package com.spring.gabi.todolist.user;
 
 import java.util.UUID;
 
-import org.springframework.boot.autoconfigure.security.SecurityProperties.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface IUserRepository extends JpaRepository<UserModel, UUID>{
